33f247b6007e3d332b59dd9dd9e8152df034caa9
Sunny Aguilar <sunny.aguilar@gmail.com>
2018-10-20 12:56:32 -0700

    add pass by value function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 
+void passByValue(int);
+void passByRef(int &);
 
 int main() {
+    int num1 = 3;
+    int *pNum = nullptr;
+    pNum = &num1;
+    *pNum = 5;
 
+    passByValue(num1);
+
+    std::cout << num1 << std::endl;
 
 
     return 0;
+}
+
+void passByValue(int val) {
+    std::cout << "Currently in passByValue function" << std::endl;
+    val++;
+    std::cout << "New value " << val << std::cout;
 }
